603b3a8e391c7b627934156592af4fc2c9116fed
Kamil Holody <kamilholody@live.com>
2023-07-21 15:17:39 +0200

    Variables and Aliases

--- cypress/integration/examples/login.spec.js
+++ cypress/integration/examples/login.spec.js
@@ -5,13 +5,15 @@ describe('Working with inputs', () => {
     })
 
     it('should fill username', () => {
-        cy.get('#user_login').clear()
-        cy.get('#user_login').type('Some invalid name', { delay: 50})
+        cy.get('#user_login').as('username')
+        cy.get('@username').clear()
+        cy.get('@username').type('Some invalid name', { delay: 50})
     })
         
     it('should fill passwrod', () => {
-        cy.get('#user_password').clear()
-        cy.get('#user_password').type('Some invalid password', { delay: 50})
+        cy.get('#user_password').as('password')
+        cy.get('@password').clear()
+        cy.get('@password').type('Some invalid password', { delay: 50})
     })
 
     it('should mark checkbox', () => {
