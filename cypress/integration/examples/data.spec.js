507e3dc905caa35dfe90697b92f8e8ad6a9e6e42
Kamil Holody <kamilholody@live.com>
2023-07-24 15:36:01 +0200

    Read Data from JSON / Local Data Files

--- cypress/integration/examples/data.spec.js
+++ cypress/integration/examples/data.spec.js
@@ -7,4 +7,12 @@ describe('Write / read Data to JSON / Text file', () => {
     it('should write data to the text file', () => {
         cy.writeFile('log.txt', 'Hello World')
     })
+
+    it('should read and verify data form JSON file', () => {
+        cy.readFile('log.json').its('age').should('eq', 25)
+    })
+
+    it('should read and verify data from the texst file', () => {
+        cy.readFile('log.txt').should('eq', 'Hello World')
+    })
 })
