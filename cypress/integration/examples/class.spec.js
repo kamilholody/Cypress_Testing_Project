e87f110a0229ee1ced863400ec09a60717fd9a8e
Kamil Holody <kamilholody@live.com>
2023-07-25 09:32:56 +0200

    Abstraction with Classes

---
+++ cypress/integration/examples/class.spec.js
@@ -0,0 +1,31 @@
+class BasePage {
+    static loadHomePAge() {
+        cy.visit('https://devexpress.github.io/testcafe/example/')
+    }
+
+    static wait(number) {
+        cy.wait(number)
+    }
+}
+
+class HomePage extends BasePage {
+    static scrollToBottom() {
+        cy.get('#submit-button').scrollIntoView()
+    }
+
+    static scrollToTop() {
+        cy.get('header').scrollIntoView()
+    }
+}
+
+
+describe('Abstraction with Classes', () => {
+    
+    it('should scroll down and up on the page', () => {
+        HomePage.loadHomePAge()
+        HomePage.scrollToBottom()
+        HomePage.wait(5000)
+        HomePage.scrollToTop()
+        HomePage.wait(3000)
+})
+})
