0f1582079751a5dbe55c817e41f03a55417e277d
Kamil Holody <kamilholody@live.com>
2023-07-27 11:29:31 +0200

    E2E Test: Searchbox

---
+++ cypress/integration/E2E/search.spec.js
@@ -0,0 +1,14 @@
+describe('Search box Test', () => {
+    before(function() {
+        cy.visit('http://zero.webappsecurity.com/index.html')
+    })
+
+    it('should type into searchbox and submit with pressing enter', () => {
+        cy.get('#searchTerm').type('some text {enter}')
+    })
+
+    it('should show search results page', () => {
+        cy.get('h2').contains('Search Results:')
+    })
+
+})
