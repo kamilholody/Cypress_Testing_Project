dec3bc4b761f45a740c4f9e70d85ca1ee7d21d6e
Kamil Holody <kamilholody@live.com>
2023-07-28 15:05:21 +0200

    E2E Test: Filter Transactions

---
+++ cypress/integration/E2E/find-transaction.spec.js
@@ -0,0 +1,23 @@
+describe('Find Transaction Test', () => {
+    before(function() {
+        cy.visit('http://zero.webappsecurity.com/index.html')
+        cy.get('#signin_button').click()
+        cy.fixture('userE2E').then(user => {
+            const username = user.id
+            const password = user.pwd
+            cy.loginE2E(username, password)
+        })
+    })
+
+    it('should filter transactions', () => {
+        cy.get('#account_activity_tab').click()
+        cy.contains('Find Transactions').click()
+        cy.get('#aa_fromAmount').type('200')
+        cy.get('#aa_toAmount').type('1000')
+        cy.get('button[type="submit"]').click()
+    })
+    it('should display results', () => {
+        cy.get('#filtered_transactions_for_account').should('be.visible')
+        cy.get('tbody > tr').its('length').should('be.gt', 0)
+    })
+})
